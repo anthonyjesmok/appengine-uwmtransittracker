4655a7cdc4beb6a9a72fe0d05a4a8516d158d55c
Anthony Jesmok <me@anthonyjesmok.com>
2015-02-23 19:46:27 -0600

    Updates for New API Structures
    Updating MCTS module for new API structure, minor updates to Jinja
    template.

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def getUWMPredictions(stop):
                 for pred in direction:
                     toPush = {
                                 'system': "University of Wisconsin-Milwaukee Parking & Transit",
-                                'route': str(child.attrib['routeTitle']) + " " + str(pred.attrib['dirTag']).title(),
+                                'route': str(child.attrib['routeTitle']) + " " + str(direction.attrib['title']).title(),
                                 'minutes': int(pred.attrib['minutes']),
                                 'stop_name': str(child.attrib['stopTitle']),
                                 'stop_num': stop
@@ -43,18 +43,19 @@ def getUWMPredictions(stop):
 def getMCTSPredictions(stops):
     times = []
     stops_csv = ','.join(map(str, stops))
-    logging.info('http://realtime.ridemcts.com/bustime/api/v1/getpredictions?key=' + mctsKey + '&stpid=' + stops_csv)
     xmlFeed = urllib2.urlopen('http://realtime.ridemcts.com/bustime/api/v1/getpredictions?key=' + mctsKey + '&stpid=' + stops_csv)
     tree = etree.parse(xmlFeed)
     root = tree.getroot()
     # Subtract 6 Hours for Central TIme
     now = datetime.datetime.now() - timedelta(hours=6)
     for child in root:
-        time_diff = datetime.datetime.strptime(child[9].text, "%Y%m%d %H:%M") - now
+        time_diff = datetime.datetime.strptime(child[10].text, "%Y%m%d %H:%M") - now
         minutes = int(time_diff.total_seconds() / 60)
+        if str(child[6].text) == "GOL":
+            child[6].text = "GoldLine"
         toPush = {
                   'system': "Milwaukee County Transit System",
-                  'route':  str(child[6].text) + " " + str(child[7].text).title() + " to " + str(child[8].text),
+                  'route':  str(child[6].text) + " " + str(child[8].text).title() + " to " + str(child[9].text),
                   'minutes': int(minutes),
                   'stop_name': str(child[2].text).title(),
                   'stop_num': int(child[3].text)
@@ -74,13 +75,16 @@ class StopHandler(webapp.RequestHandler):
     def get(self):
         requested_stop = self.request.get('id')
         stops = parseStops()
+        raw_times = []
         times = []
         for stop in stops:
             if stop['uwm_tag'] == requested_stop:
-                times = getUWMPredictions(stop['uwm_id']) + getMCTSPredictions(stop['mcts'])
+                raw_times = getUWMPredictions(stop['uwm_id']) + getMCTSPredictions(stop['mcts'])
                 break
-        times = sorted(times, key=lambda k: k['minutes']) 
-        logging.info(times)
+        raw_times = sorted(raw_times, key=lambda k: k['minutes'])
+        for idx in range(len(raw_times)):
+            if idx > 0 and raw_times[idx-1]['minutes'] != raw_times[idx]['minutes'] and raw_times[idx-1]['route'] != raw_times[idx]['route']:
+                times.append(raw_times[idx])
         template = jinja_environment.get_template('index.html')
         self.response.out.write(template.render({'stops': stops, 'times': times}))
         
